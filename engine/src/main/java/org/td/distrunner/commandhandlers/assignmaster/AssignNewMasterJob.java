78c50e0eab4f9e62369cc816f9934ca3c12a691c
tayfundogdas <tayfundogdas@DESKTOP-GJO48LB>
2017-03-17 21:49:11 +0100

    Some comments fixes

--- engine/src/main/java/org/td/distrunner/commandhandlers/assignmaster/AssignNewMasterJob.java
+++ engine/src/main/java/org/td/distrunner/commandhandlers/assignmaster/AssignNewMasterJob.java
@@ -4,19 +4,19 @@ public class AssignNewMasterJob {
 
 	//assign new master among master candidates stated in settings
 	
-	// in case of heartBeats fails for threshold according to jobcount*waittimeconstant
+	// in case of heartBeats fails for threshold send new request according to jobcount*waittimeconstant
 	public void broadcastNewMasterRequesttoMasterCandidates()
 	{
 		
 	}
 	
-	//if new master request already taken
+	//if new master request already taken cancel my request
 	public void cancelMyNewMasterRequesttoMasterCandidatesMessage()
 	{
 		
 	}
 	
-	//after new master selected it notify all nodes
+	//after new master selected notify all nodes
 	public void broadcastNewMasterMessagetoNodes()
 	{
 		
